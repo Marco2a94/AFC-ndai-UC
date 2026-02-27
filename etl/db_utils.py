e624b98cd5df8ea45e47f080de6de08e59238580
Marco2a94 <102526094+Marco2a94@users.noreply.github.com>
2026-02-27 03:09:55 +0100

    refactor database connection: update host to use environment variable for better configuration management

--- etl/db_utils.py
+++ etl/db_utils.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
+        host=os.getenv("POSTGRES_HOST"),
         database=os.getenv("POSTGRES_DB"),
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD")
